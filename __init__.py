f893c20426ed9e0d909836cffaf1c492a30a4230
Onionman61 <769950999@qq.com>
2025-11-28 15:44:27 +0800

    Add files via upload
    更新版本，现在可支持调用FLUX2.dev

--- __init__.py
+++ __init__.py
@@ -1,14 +1,13 @@
 # __init__.py
 
-from .kontext_api import ModelScopeKontextAPI
+from .kontext_api import ModelScopeUniversalAPI
 
 # A dictionary that contains all nodes you want to export with their names
-# The names should be descriptive and searchable in the frontend
 NODE_CLASS_MAPPINGS = {
-    "ModelScopeKontextAPI": ModelScopeKontextAPI
+    "ModelScopeUniversalAPI": ModelScopeUniversalAPI
 }
 
 # A dictionary that contains the friendly/humanly readable titles for the nodes
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "ModelScopeKontextAPI": "ModelScope Kontext API"
+    "ModelScopeUniversalAPI": "ModelScope Universal API"
 }
